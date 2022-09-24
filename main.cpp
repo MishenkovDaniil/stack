f0b8288e5d04205dd400a2d7cb79cc9ae4dd8f00
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-09-24 17:01:32 +0300

    create main.cpp with calling stack functions and with some defining to do logs

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "stack.h"
+
+
+#define stack_push(A,B) stack_push (A, B, #A, __PRETTY_FUNCTION__, __FILE__, __LINE__)
+#define stack_pop(A,B)  stack_pop  (A, #A, __PRETTY_FUNCTION__, __FILE__, __LINE__, B);
+#define stack_pop(A)    stack_pop  (A, #A, __PRETTY_FUNCTION__, __FILE__, __LINE__);
+
+int main ()
+{
+    Stack stk1 = {};
+
+    stack_init (&stk1, 5);
+    stack_push (&stk1, 1);
+    stack_push (&stk1, 2);
+    stack_push (&stk1, 3);
+    stack_pop (&stk1);
+    stack_pop (&stk1);
+    stack_push (&stk1, 2);
+    stack_push (&stk1, 3);
+
+    for (int i = 0; i < stk1.capacity; i++)
+    {
+        printf ("[%lf]\t", stk1.data[i]);
+    }
+
+    stack_dtor (&stk1);
+
+    return 0;
+
+}
