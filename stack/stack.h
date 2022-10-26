8eeb2bdd258d2fac0d3be8c7922ecc9291687d0a
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-10-26 21:51:46 +0300

    fix bug with resize (if...if => if...else if)

--- stack/stack.h
+++ stack/stack.h
@@ -7,8 +7,7 @@
 
 #include <stdlib.h>
 #include <assert.h>
-
-//double check!!!
+#include <windows.h>
 
 #define CANARY_PROT 1 // state value for turning on canary protection of stack and stack data
 #define HASH_PROT 2   // state value for turning on hash protection of stack and stack data
@@ -22,20 +21,21 @@
 #endif
 
 typedef unsigned long long canary_t; // sets canary type
-typedef double elem_t;               // sets type of data elements
+typedef int elem_t;               // sets type of data elements
 
 
 static int ERRNO = 0;                              // sets a "non-error" value
 static const int CANARIES_NUMBER = 2;              // sets a number of "canaries"
 static const size_t POISON = 0xDEADBEEF;           // sets "poison" value (a value to indicate errors in stack data values)
 static const canary_t CANARY = 0xAB8EACAAAB8EACAA; // sets value of "canary" (a value to indicate safety of stack and stack data)
+static const int START_CAPACITY = 10;
 
 enum errors
 {
     STACK_FOPEN_FAILED   = 0x1 << 0,
     STACK_ALLOC_FAIL     = 0x1 << 1,
-    STACK_NULL_STK       = 0x1 << 2,
-    STACK_NULL_DATA      = 0x1 << 3,
+    STACK_BAD_READ_STK   = 0x1 << 2,
+    STACK_BAD_READ_DATA  = 0x1 << 3,
     STACK_STACK_OVERFLOW = 0x1 << 4,
     STACK_INCORRECT_SIZE = 0x1 << 5,
     STACK_VIOLATED_DATA  = 0x1 << 6,
@@ -107,7 +107,7 @@ int    stack_push (Stack *stk, elem_t value, int *err = &ERRNO);
  */
 elem_t stack_pop  (Stack *stk,               int *err = &ERRNO);
 
-void   __debug_stack_init (Stack *stk, int capacity, const char *stk_name, const char *call_func, const int call_line,
+int   __debug_stack_init (Stack *stk, int capacity, const char *stk_name, const char *call_func, const int call_line,
                                                      const char *call_file, const int creat_line, int *err = &ERRNO);
 int    __debug_stack_push (Stack *stk, elem_t value, const int call_line, int *err = &ERRNO);
 elem_t __debug_stack_pop  (Stack *stk,               const int call_line, int *err = &ERRNO);
@@ -123,13 +123,32 @@ static void   log_status       (Stack *stk, int *err, FILE *file = log_file);
 static void   log_info         (Stack *stk, int *err, FILE *file = log_file);
 static void   log_data         (Stack *stk, FILE *file = log_file);
 static void   log_data_members (Stack *stk, FILE *file = log_file);
+int is_bad_read_ptr (void *p);
+
+
+int is_bad_read_ptr (void *p)
+{
+    MEMORY_BASIC_INFORMATION mbi = {};
+
+    const int PROTECT_MASK = PAGE_EXECUTE | PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY;
+    if (!(VirtualQuery (p, &mbi, sizeof (mbi))))
+    {
+        return 1;
+    }
+    else if (!(mbi.Protect & PROTECT_MASK))
+    {
+        return 1;
+    }
+
+    return 0;
+}
 
 static int stack_realloc (Stack *stk, int previous_capacity, int *err)
 {
     assert (stk);
     assert (err);
 
-    if (!(stk && err) || previous_capacity < 0)
+    if (is_bad_read_ptr(stk) || err == nullptr || previous_capacity < 0)
     {
         printf ("ERROR: stack pointer or error pointer is a nullptr or previous capacity at func stack_realloc is under zero\n");
     }
@@ -174,10 +193,6 @@ static int stack_realloc (Stack *stk, int previous_capacity, int *err)
         #endif
     }
 
-    #ifdef HASH_PROT
-    stk->hash_sum = m_gnu_hash (stk->data, stk->capacity * sizeof (elem_t));
-    #endif
-
     if (stk->data == nullptr)
     {
         *err |= STACK_ALLOC_FAIL;
@@ -205,10 +220,6 @@ void fill_stack (Stack *stk, int start, int *err)
     {
         (stk->data)[i] = POISON;
     }
-
-    #ifdef HASH_PROT
-    stk->hash_sum = m_gnu_hash (stk->data, stk->capacity * sizeof (elem_t));
-    #endif
 }
 
 int stack_push (Stack *stk, elem_t value, int *err)
@@ -221,12 +232,7 @@ int stack_push (Stack *stk, elem_t value, int *err)
         err = &ERRNO;
     }
 
-    if (stack_error (stk, err, 0))
-    {
-        #ifdef STACK_DEBUG
-        stack_dump (stk, err);
-        #endif
-    }
+    stack_error (stk, err);
 
     if (*err)
     {
@@ -250,6 +256,14 @@ int stack_push (Stack *stk, elem_t value, int *err)
 
 int __debug_stack_push (Stack *stk, elem_t value, const int call_line, int *err)
 {
+    if (stack_error (stk, err, 0))
+    {
+        #ifdef STACK_DEBUG
+        stack_dump (stk, err);
+        #endif
+
+        return POISON;
+    }
     (stk->info).file = __FILE__;
     (stk->info).func = __PRETTY_FUNCTION__;
     (stk->info).line = __LINE__;
@@ -268,12 +282,7 @@ elem_t stack_pop (Stack *stk, int *err)
         err = &ERRNO;
     }
 
-    if (stack_error (stk, err, 0))
-    {
-        #ifdef STACK_DEBUG
-        stack_dump (stk, err);
-        #endif
-    }
+    stack_error (stk, err);
 
     if (*err)
     {
@@ -308,6 +317,14 @@ elem_t stack_pop (Stack *stk, int *err)
 
 elem_t __debug_stack_pop (Stack *stk, const int call_line, int *err)
 {
+    if (stack_error (stk, err, 0))
+    {
+        #ifdef STACK_DEBUG
+        stack_dump (stk, err);
+        #endif
+
+        return POISON;
+    }
     (stk->info).file = __FILE__;
     (stk->info).func = __PRETTY_FUNCTION__;
     (stk->info).line = __LINE__;
@@ -326,6 +343,12 @@ int stack_init (Stack *stk, int capacity, int *err)
         err = &ERRNO;
     }
 
+    if (is_bad_read_ptr (stk) || capacity <= 0)
+    {
+        stack_error (stk, err);
+        return *err;
+    }
+
     stk->capacity = capacity;
 
     if (!(stack_realloc (stk, 0, err)))
@@ -341,12 +364,17 @@ int stack_init (Stack *stk, int capacity, int *err)
 
     stack_error (stk, err);
 
-    return 0;
+    return *err;
 }
 
-void __debug_stack_init (Stack *stk, int capacity, const char *stk_name, const char *call_func, const int call_line,
+int __debug_stack_init (Stack *stk, int capacity, const char *stk_name, const char *call_func, const int call_line,
                          const char *call_file, const int creat_line, int *err)
 {
+    if (is_bad_read_ptr (stk))
+    {
+        stack_error (stk, err);
+        return *err;
+    }
     (stk->info).call_func = call_func;
     (stk->info).call_file = call_file;
     (stk->info).creat_line = creat_line;
@@ -358,14 +386,14 @@ void __debug_stack_init (Stack *stk, int capacity, const char *stk_name, const c
 
     (*(stk_name) != '&') ? (stk->info).stk_name = stk_name : (stk->info).stk_name = stk_name + 1;
 
-    stack_init (stk, capacity, err);
+    return stack_init (stk, capacity, err);
 }
 
 static void stack_resize (Stack *stk, int *err)
 {
     assert (stk && stk->data);
 
-    if (stack_error (stk, err, 0))
+    if (stack_error (stk, err, 0)) //defin-ом
     {
         #ifdef STACK_DEBUG
         stack_dump (stk, err);
@@ -384,7 +412,7 @@ static void stack_resize (Stack *stk, int *err)
             stack_realloc (stk, previous_capacity, err);
             fill_stack    (stk, previous_capacity, err);
         }
-        if (stk->capacity > current_size * 4)
+        else if (stk->capacity > current_size * 4 && previous_capacity > 10)
         {
             stk->capacity /= 2;
 
@@ -392,11 +420,15 @@ static void stack_resize (Stack *stk, int *err)
         }
     }
 
-    if (stack_error (stk, err, 0))
+    if (stack_error (stk, err, 0) != STACK_DATA_MESSED_UP)
     {
         #ifdef STACK_DEBUG
         stack_dump (stk, err);
         #endif
+    }
+    else
+    {
+        *err = 0;
     }
 }
 
@@ -405,26 +437,32 @@ static int stack_error (Stack *stk, int *err, int need_in_dump)
     assert (stk);
     assert (err);
 
+    do
+    {
     if (!log_file)
     {
         *err |= STACK_FOPEN_FAILED;
-        #ifdef STACK_DEBUG
-        stack_dump (stk, err, stderr);
-        #endif
+
+        break;
     }
-    if (!stk)
+    if (is_bad_read_ptr (stk))
     {
-        *err |= STACK_NULL_STK;
+        fprintf (stderr, "stk is a bad ptr\n");
+        *err |= STACK_BAD_READ_STK;
+
+        break;
     }
-    if (!stk->data)
+    if (is_bad_read_ptr (stk->data))
     {
-        *err |= STACK_NULL_DATA;
+        *err |= STACK_BAD_READ_DATA;
+
+        break;
     }
     if (stk->size > stk->capacity)
     {
         *err |= STACK_STACK_OVERFLOW;
     }
-    if (stk->size < 0 || stk->capacity < 0)
+    if (stk->size < 0 || stk->capacity <= 0)
     {
         *err |= STACK_INCORRECT_SIZE;
     }
@@ -451,6 +489,7 @@ static int stack_error (Stack *stk, int *err, int need_in_dump)
     }
 
     #endif
+    } while (0);
 
     #ifdef STACK_DEBUG
     if (need_in_dump)
@@ -464,6 +503,11 @@ static int stack_error (Stack *stk, int *err, int need_in_dump)
 
 static void stack_dump (Stack *stk, int *err, FILE *file)
 {
+    if (*err & STACK_BAD_READ_DATA)
+    {
+        fprintf (stderr, "data is a bad ptr\n");
+    }
+
     assert (stk && stk->data);
     assert (err);
     assert (file);
@@ -481,7 +525,7 @@ static void stack_dump (Stack *stk, int *err, FILE *file)
 
 void stack_dtor (Stack *stk)
 {
-    assert (stk && stk->data);
+    //assert (stk && stk->data);
 
     if (stk && stk->data)
     {
@@ -491,6 +535,10 @@ void stack_dtor (Stack *stk)
 
         stk->data = nullptr;
         stk = nullptr;
+    }
+    else
+    {
+        printf ("nothing to dtor");
     }
 }
 
@@ -533,14 +581,6 @@ static void log_status (Stack *stk, int *err, FILE *file)
         if (*err & STACK_ALLOC_FAIL)
         {
             status[index++] = "memory allocation failed";
-        }
-        if (*err & STACK_NULL_STK)
-        {
-            status[index++] = "stk is a null pointer";
-        }
-        if (*err & STACK_NULL_DATA)
-        {
-            status[index++] = "data is a null pointer";
         }
         if ((*err) & STACK_STACK_OVERFLOW)
         {
@@ -607,19 +647,19 @@ void log_data_members (Stack *stk, FILE *file)
 
     for (int i = 0; i < stk->size; i++)
     {
-        fprintf (file, "\t*[%ld""] = %lf\n", i, (stk->data)[i]);
+        fprintf (file, "\t*[%ld""] = %d\n", i, (stk->data)[i]);
     }
     for (int i = stk->size; i < stk->capacity; i++)
     {
-        fprintf (file, "\t [%ld] = %lf\n", i, (stk->data)[i]);
+        fprintf (file, "\t [%ld] = %d\n", i, (stk->data)[i]);
     }
 }
 
 #ifdef STACK_DEBUG
 
-#define stack_init(stk, capacity, err)     __debug_stack_init (stk, capacity, #stk, __PRETTY_FUNCTION__, __LINE__, __FILE__, __LINE__, err)
-#define stack_push(stk, value, err)        __debug_stack_push (stk, value, __LINE__, err)
-#define stack_pop(stk, err)               __debug_stack_pop  (stk, __LINE__, err)
+#define stack_init(stk, capacity)     __debug_stack_init (stk, capacity, #stk, __PRETTY_FUNCTION__, __LINE__, __FILE__, __LINE__)
+#define stack_push(stk, value)        __debug_stack_push (stk, value, __LINE__)
+#define stack_pop(stk)               __debug_stack_pop  (stk, __LINE__)
 
 #endif
 
